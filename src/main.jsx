fdb1473f34c5bd21b324f0bb212d73f920be69db
unknown <oualizakaria5@gmail.com>
2024-12-27 16:46:10 +0100

    final

---
+++ src/main.jsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import EditContact ,{
+  action as editAction,
+}from "./routes/edit";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./erreur-page";
+import Contact,{loader as contactLoader,  action as contactAction,} from "./routes/contact";
+import { loader as rootLoader ,  action as rootAction,} from "./routes/root";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+        action: contactAction,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+    
+  },
+  
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        Welcome to Contacts by Zakaria
+        <br />
+        Check out my GitHub{" "}
+        <a href="https://github.com/zakariaouali">
+        GITHUB
+        </a>
+        .
+      </p>
+    );
+  }
+  
